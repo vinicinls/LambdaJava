7cce5ffa65f37daa777582f013cdccd0b22e20fa
vinicinls <viniciuslacerda437@gmail.com>
2025-03-01 17:56:09 -0300

    eu amo estudar

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,30 +2,33 @@ package application;
 
 import entities.Product;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.*;
 
 public class Program {
+
+   /* public static double compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
+    }*/
+
     public static void main(String[] args) {
 
         List<Product> list = new ArrayList<>();
+        list.add(new Product("TV", 900.0));
+        list.add(new Product("Mesa", 1100.0));
+        list.add(new Product("Mesa", 4100.0));
+        list.add(new Product("Mesa", 6100.0));
 
-            list.add(new Product("mesa",1000.0));
-            list.add(new Product("banco",1000.0));
-            list.add(new Product("tv",1000.0));
-            list.add(new Product("quadro",1000.0));
-            list.add(new Product("armario",1000.0));
+        list.sort((p1,p2) -> p1.getPrice().compareTo(p2.getPrice()));
+        list.forEach(System.out::println);
 
-            list.sort((p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+        double sum = list.stream().mapToDouble(Product::getPrice).sum();
+        System.out.println(sum);
 
-            System.out.println(list.size());
-            System.out.println("-------------");
 
-            for (Product p : list){
-                System.out.println(p);
-            }
+
+        }
 
     }
-}
+
+
+
