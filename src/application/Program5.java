1ac6088418ad579a53a0241766d06de9afadc805
vinicinls <viniciuslacerda437@gmail.com>
2025-03-03 12:37:19 -0300

    fibonacci

---
+++ src/application/Program5.java
@@ -0,0 +1,34 @@
+package application;
+
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Program5 {
+    public static void main(String[] args) {
+
+
+        List<Integer> list = Arrays.asList(3,4,5,10,7);
+
+        Stream<Integer> st1 = list.stream().map(x -> x * 10);
+
+        System.out.println(Arrays.toString(st1.toArray()));
+
+        System.out.println();
+        Stream<String> st2 = Stream.of("Vinicius","Chocolate","Enzo").map(x -> x.toUpperCase());
+        System.out.println(Arrays.toString(st2.toArray()));
+
+        Stream<Integer> st3 = Stream.iterate(0, x -> x+2);
+        System.out.println(Arrays.toString(st3.limit(11).toArray()));
+
+        //sequencia de fibonacci
+
+        Stream<Long> st4 = Stream.iterate(new Long[]{0L,1L},p-> new Long[]{p[1], p[0] + p[1]}).map(p -> p[0]);
+        System.out.println(Arrays.toString(st4.limit(10).toArray()));
+
+
+
+
+    }
+}
